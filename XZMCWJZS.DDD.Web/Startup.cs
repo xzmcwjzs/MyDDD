76403e8f1db42a174397d97d52480c472778c09f
xzmcwjzs <1911805752@qq.com>
2018-01-04 10:43:20 +0800

    20180104

---
+++ XZMCWJZS.DDD.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XZMCWJZS.DDD.Web.Startup))]
+namespace XZMCWJZS.DDD.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
