9b76fb07f0410a43e94363fde8269a81024baba2
xzmcwjzs <1911805752@qq.com>
2018-01-06 21:15:32 +0800

    UnitOfWork

--- XZMCWJZS.DDD.Repository/EntityFramework/EntityFrameworkRepositoryContext.cs
+++ XZMCWJZS.DDD.Repository/EntityFramework/EntityFrameworkRepositoryContext.cs
@@ -1,17 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using XZMCWJZS.DDD.Domain.Repositories;
 
 namespace XZMCWJZS.DDD.Repository.EntityFramework
 {
     public class EntityFrameworkRepositoryContext : IEntityFrameworkRepositoryContext
     {
-        // 引用我们定义的OnlineStoreDbContext类对象
+        // ThreadLocal代表线程本地存储，主要相当于一个静态变量
+        // 但静态变量在多线程访问时需要显式使用线程同步技术。
+        // 使用ThreadLocal变量，每个线程都会一个拷贝，从而避免了线程同步带来的性能开销
+
+        private readonly ThreadLocal<OnlineStoreDbContext> localCtx = new ThreadLocal<OnlineStoreDbContext>(() => new OnlineStoreDbContext());
         public OnlineStoreDbContext DbContex
         {
-            get { return new OnlineStoreDbContext(); }
+            get { return localCtx.Value; }
+        }
+        private readonly Guid id = Guid.NewGuid();
+        public Guid Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public void Commit()
+        {
+            var validationError = localCtx.Value.GetValidationErrors();
+            localCtx.Value.SaveChanges();
+        }
+
+        public void RegisterNew<TAggregateRoot>(TAggregateRoot entity) where TAggregateRoot : class, Domain.IAggregateRoot
+        {
+            localCtx.Value.Set<TAggregateRoot>().Add(entity);
+        }
+
+        public void RegisterModified<TAggregateRoot>(TAggregateRoot entity) where TAggregateRoot : class, Domain.IAggregateRoot
+        {
+            localCtx.Value.Entry<TAggregateRoot>(entity).State = EntityState.Modified;
+        }
+
+        public void RegisterDeleted<TAggregateRoot>(TAggregateRoot entity) where TAggregateRoot : class, Domain.IAggregateRoot
+        {
+            localCtx.Value.Set<TAggregateRoot>().Remove(entity);
         }
     }
 }
